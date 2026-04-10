a7382f9c96aed56dda35c6b888cd3b392940df67
alessiogandelli <alessiogandelli99@gmail.com>
2026-04-10 12:17:51 +0200

    fix base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   output: 'static',
-  site: 'https://alessiogandelli.github.io',
-  base: '/comuni-brescia',
+  site: 'https://ranturismo.com',
+  base: '/',
 });
